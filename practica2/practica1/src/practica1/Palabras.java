880e7142a576cfb3aadd6ce5e80da722a9f36c1d
Valenz23 <pva_89@hotmail.com>
2017-10-20 12:14:38 +0200

    añadiendo graficas
    son dos archivos ods con las graficas de regresion

--- practica2/practica1/src/practica1/Palabras.java
+++ practica2/practica1/src/practica1/Palabras.java
@@ -1,13 +1,9 @@
-/*
+/*0
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package practica1;
-<<<<<<< HEAD:practica2/practica/src/practica1/Palabras.java
-
-=======
->>>>>>> 7771cf9170dbd30f559b3ee659c3153b6fd2a7f8:practica1/src/practica1/Palabras.java
 /**
  *
  * @author Franelas
