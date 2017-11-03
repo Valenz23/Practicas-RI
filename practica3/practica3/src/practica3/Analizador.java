80d09e00724a8e3f9f736e53abc7c630120d85d1
Franelas <fj.maldonado.ugr@gmail.com>
2017-11-03 12:01:44 +0100

    oloasd
    oasdlams

--- practica3/practica3/src/practica3/Analizador.java
+++ practica3/practica3/src/practica3/Analizador.java
@@ -34,7 +34,7 @@ public class Analizador {
                 //.addTokenFilter()
                 .addTokenFilter(LowerCaseFilterFactory.class)
                 .addTokenFilter(StopFilterFactory.class)
-                .addCharFilter(PatternReplaceCharFilterFactory.class, "[a-z]", "a")
+                .addCharFilter("[a-z]", "a")
                 .build();
     }
     
